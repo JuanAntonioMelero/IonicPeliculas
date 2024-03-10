d5281f1c65da84dfe804bec438282af09d9ce2a8
Juan Antonio Melero <juanantoniomelero@Mac-mini-de-Juan.local>
2024-03-10 12:34:04 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '004ab1782f4b8e99a9634759a814aa4a',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  url: 'https://api.themoviedb.org/3',
+  apiKey: '004ab1782f4b8e99a9634759a814aa4a',
+  imgPath: 'https://image.tmdb.org/t/p'
 };
 
 /*
